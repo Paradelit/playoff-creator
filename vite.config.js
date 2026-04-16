1956ad79346d2c1bc8e0f877c0c814612f0c1409
Sergio Paradela <serpa2003@gmail.com>
2026-04-17 00:46:42 +0200

    chore: fix vitest exclude config for main

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     environment: 'happy-dom',
     globals: true,
     setupFiles: ['./src/test/setup.js'],
+    exclude: ['node_modules', 'dist', 'functions/**'],
   },
   server: {
     headers: {
